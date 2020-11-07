586e6acc03c4a0a4d06c4725d6e7ecd3394d6ff0
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-07 14:59:53 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan39-Nilai/src/pboif2/pkg10119048/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class Petugas {
+    private String nama;
+    
+    public String getNama(){
+       return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+}
